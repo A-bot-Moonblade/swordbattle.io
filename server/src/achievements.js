03e4d15614c7e73c408b20b2255a44e20c8c349f
A-bot-Moonblade <moonblade696@gmail.com>
2025-03-12 18:30:43 -0700

    Achievements Attempt 1

---
+++ server/src/achievements.js
@@ -0,0 +1,20 @@
+function Achievement(acheivementname, acheivementproperty, acheivementvalue, acheivementtext, acheivementpoints){
+    this.acheivementname = acheivementname;
+    this.acheivementproperty = acheivementproperty;
+    this.acheivementvalue = acheivementvalue;
+    this.acheivementtext = acheivementtext;
+  }
+  achievements.push(new Achievement("Gem Newbie", account.gems, 100, "Earn 100 gems!");
+  achievements.push(new Achievement("Gem Seeker", account.gems, 1000, "Earn 1,000 gems!");
+  achievements.push(new Achievement("Gem Gatherer", account.gems, 10000, "Earn 10,000 gems!");
+  achievements.push(new Achievement("Gem Collecter", account.gems, 100000, "Earn 100,000 gems!");
+  achievements.push(new Achievement("Gem Hoarder", account.gems, 100000, "Earn 1,000,000 gems!");
+  achievements.push(new Achievement("First Blood", account.kills, 1, "Kill 1 player!");
+  achievements.push(new Achievement("Young Fighter", account.kills, 5, "Kill 5 players!");
+  achievements.push(new Achievement("Rookie", account.kills, 10, "Kill 10 players!");
+  achievements.push(new Achievement("Warrior", account.kills, 100, "Kill 100 players!");
+  achievements.push(new Achievement("Slayer", account.kills, 1000, "Kill 1,000 players!");
+  achievements.push(new Achievement("Assassin", account.kills, 10000, "Kill 10,000 players!");
+  achievements.push(new Achievement("Prodigy", account.kills, 100000, "Kill 100,000 players!");
+  achievements.push(new Achievement("Destroyer", account.kills, 1000000, "Kill 1,000,000 players!");
+  achievements.push(new Achievement("Terminator", account.kills, 10000000, "Kill 10,000,000 players!");
